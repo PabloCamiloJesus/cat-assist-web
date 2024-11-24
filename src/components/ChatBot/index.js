3f60b91caf7d4c0dee20062c1ddb1e5e05c2b206
Fhelipe Alves Mantovan <fhelipe.mantovan007@gmail.com>
2024-11-24 10:53:06 -0300

    Diminuido a topK do chatbot para 40 (de 64)

--- src/components/ChatBot/index.js
+++ src/components/ChatBot/index.js
@@ -37,7 +37,7 @@ const ChatBot = () => {
   const generationConfig = {
     temperature: 1,
     topP: 0.95,
-    topK: 64,
+    topK: 40,
     maxOutputTokens: 8192,
     responseMimeType: "text/plain",
   };
