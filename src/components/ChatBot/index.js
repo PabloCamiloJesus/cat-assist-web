24bad99ee6d9729674ddbb966617015a7353f250
Fhelipe Alves Mantovan <fhelipe.mantovan007@gmail.com>
2025-11-30 23:11:57 -0300

    Modelo da IA trocado para gemini-2.5-flash
    Para arrumar problema com a gemini-1.5-flash retornando inválido

--- src/components/ChatBot/index.js
+++ src/components/ChatBot/index.js
@@ -31,7 +31,7 @@ const ChatBot = () => {
   const genAI = new GoogleGenerativeAI(apiKey);
 
   const model = genAI.getGenerativeModel({
-    model: "gemini-1.5-flash",
+    model: "gemini-2.5-flash",
   });
 
   const generationConfig = {
